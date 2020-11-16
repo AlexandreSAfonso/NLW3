b66dc5752a77debf6dd488172cbbc165420448b9
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2020-11-16 16:03:08 -0300

    map layout
    adj map poupup layout

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,9 +1,30 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+//create object map
+var map = L.map('mapid').setView([-22.569287,-47.4202659,], 15);
 
+//create and add titleLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+//create icon
+const icon = L.icon({
+    iconUrl: "../public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2] 
+})
+//create poupup overlay
+const popup = L.popup({
+    clouseButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('lar das Meninas <a href="orphanage.html?id=1" class="chose-orphanage"> <img src="./public/images/arrow-white.svb"</a>')
+
+//create and add marker
+/*L.marker([-22.569287,-47.4202659,]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    .openPopup();*/
+L.marker([-22.569287,-47.4202659,], {icon})
+    .addTo(map)
+    .bindPopup(popup)
