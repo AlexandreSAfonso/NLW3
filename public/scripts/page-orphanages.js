d019e712db9d1a108530f9d87e80cf3fd7cba4ad
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2020-11-17 16:13:23 -0300

    map layout end
    finalization of the map page
    small Popup pending without the icon

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,20 +6,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
 //create icon
 const icon = L.icon({
-    iconUrl: "../public/images/map-marker.svg",
+    iconUrl: "./public/images/map-marker.svg",
     iconSize: [58,68],
     iconAnchor: [29,68],
     popupAnchor: [170,2] 
 })
 //create poupup overlay
 const popup = L.popup({
-    clouseButton: false,
+    closeButton: false,
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('lar das Meninas <a href="orphanage.html?id=1" class="chose-orphanage"> <img src="./public/images/arrow-white.svb"</a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg" /></a>')
 
 //create and add marker
 /*L.marker([-22.569287,-47.4202659,]).addTo(map)
